de2ea97cece3c18da8b22940b0d87b50c6320aa5
Vadim Chaykin <qaziro@gmail.com>
2022-12-20 17:42:19 +0200

    Initial Commit

---
+++ RocketX/RocketXApp.swift
@@ -0,0 +1,17 @@
+//
+//  RocketXApp.swift
+//  RocketX
+//
+//  Created by qaz on 20.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct RocketXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
